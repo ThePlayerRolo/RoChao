f0a57b2bb763725e6e644c34e343068ae75dcc8f
ThePlayerRolo <thesolis78243@gmail.com>
2025-04-27 14:14:21 -0400

    Header and Footer Seperation

--- Default.php
+++ Default.php
@@ -5,34 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <style>
-    .TopBarLinks {
-    text-decoration: none; 
-    color:white; 
-    font-size:20px;
-    position: relative;
-    bottom: -3px;
-    left: -10px;
-    font-family: "Comic Sans MS", "Comic Sans", cursive;
-    }
-    .TopBarLinks2 {
-    text-decoration: none; 
-    color:white; 
-    font-size:17px;
-    position: relative;
-    bottom: -3px;
-    left: -10px;
-    font-family: "Comic Sans MS", "Comic Sans", cursive;
-    }
-    .TopBarDiv {
-        width: 1024px;
-        height: 35px;
-    background-color:#6e99c9; 
-    position: absolute; 
-    top: 87px; 
-    left:380px;
-    text-align: center;
-    border-style:solid;
-    }
     .ComicSansVirtualPlayWorld {
         font-family: "Comic Sans MS", "Comic Sans", cursive;
         position: relative;
@@ -46,27 +18,6 @@
         left:10px;
         top:-25px;
 
-    }
-    .TopBarDiv2 {
-        width: 1024px;
-        height: 35px;
-    background-color:black; 
-    position: absolute; 
-    top: 125px; 
-    left:380px;
-    text-align: center;
-    border-style:solid;
-    }
-    .TopBanner {
-        width: 1024px; 
-        height: 83px; 
-        margin-bottom: 0px; 
-        position: absolute; 
-        left: 380px; 
-        top: -1px;
-        border-style:solid;
-        z-index: 1;
-        
     }
     .VirtualPlayworld {
         width: 768px;
@@ -88,49 +39,10 @@
         bottom: 45px;
 
     }
-    .BottomTextDiv {
-        position: relative;
-        bottom: -700px;
-        
-    }
-    .BottomTextTop {
-        text-align: center;
-        font-size:12px;
-        
-    }
-    .BottomTextMiddle {
-        text-align: center;
-        position: relative;
-        font-size:12px;
-        bottom: 35px;
-    }
-    .BottomText {
-        text-align: center;
-        position: relative;
-        font-size:12px;
-        bottom: 45px;
-    }
     .VirtualPlayworldVideo {
         position: relative;
         left:335px;
         bottom: -80px;
-    }
-    .TopLogo {
-        width:425px;
-        height:70px;
-        position: relative;
-        left: 650px;
-        top: 0px;
-        z-index: 2;
-        
-    }
-    .DownloadButton {
-        width: 163px;
-        height: 36px;
-        position: relative;
-        top: -55px;
-        left: 1220px;
-        z-index: 3;
     }
     .VirtualICons {
         height: 52px; 
@@ -151,31 +63,7 @@
 </style>
 </head>
 <body>
-    <img alt="RoChaoTopBanner" src="/images/TopLanding.png" class = "TopBanner" />
-    <a href="https://www.w3.org/" > <img src="/images/Logo.png" alt="Logo" class = "TopLogo"></a>
-    <div>
-    <a href="https://www.w3.org/" > <img src="/images/PlayButtonLanding.png" alt="Play!" class = "DownloadButton"></a>
-    </div>
-    <div  class = "TopBarDiv">
-
-    <a href="https://www.w3.org/ " class ="TopBarLinks">MY ROBLOX |</a> 
-    <a href="https://www.w3.org/" class = "TopBarLinks"> Games |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks"> Catalog |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks"> People |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks"> Fourms |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks"> News |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks"> Help |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks"> Chao? </a>
-    </div>
-    <div  class = "TopBarDiv2">
-    <a href="https://www.w3.org/" class = "TopBarLinks2"> Character |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks2"> Profile |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks2"> Friends |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks2"> Groups |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks2"> Create |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks2"> Settings |</a>
-    <a href="https://www.w3.org/" class = "TopBarLinks2"> Notifications</a>
-    </div>
+    <?php include 'Header.php';?>
     <div class= "VirtualPlayworld">
         <h1 class = "ComicSansVirtualPlayWorld">ROCHAO Virtual Playworld</h1>
         <h3 class = "ComicSansVirtualPlayWorld2">ROCHAO is Free!</h3>
@@ -186,15 +74,7 @@
 
     </div>
     <div class = "BottomTextDiv">
-    <hr>
-    <p class="BottomTextTop">ROCHAO is not affiliated with Roblox Corp., Lego, Megablocks, Sega, Nintendo, Pokemon, Lincoln Logs, Yu Gi Oh, K'Nex, TinkerToys, Erector Set, or The Pirates Of The Caribbean. ARrrr! </p> <br>
-    <p class = "BottomTextMiddle"> Use of this site signifies your acceptance of the <a href="https://www.w3.org/">Terms and Conditions</a></p>
-    <p class = "BottomText">
-         <a href="https://www.w3.org/">Privacy Policy |</a> 
-        <a href="https://www.w3.org/"> Contact Us |</a>
-        <a href="https://www.w3.org/"> About Us </a>
-
-    </div>
+    <?php include 'Footer.php';?>
 
 </body>
 </html>
---
+++ Footer.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>ROCHAO - Virtual Playworld</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<style>
+    .BottomTextDiv {
+        position: relative;
+        bottom: -700px;
+        
+    }
+    .BottomTextTop {
+        text-align: center;
+        font-size:12px;
+        
+    }
+    .BottomTextMiddle {
+        text-align: center;
+        position: relative;
+        font-size:12px;
+        bottom: 35px;
+    }
+    .BottomText {
+        text-align: center;
+        position: relative;
+        font-size:12px;
+        bottom: 45px;
+    }
+</style>
+</head>
+<body>
+    <hr>
+    <p class="BottomTextTop">ROCHAO is not affiliated with Roblox Corp., Lego, Megablocks, Sega, Nintendo, Pokemon, Lincoln Logs, Yu Gi Oh, K'Nex, TinkerToys, Erector Set, or The Pirates Of The Caribbean. ARrrr! </p> <br>
+    <p class = "BottomTextMiddle"> Use of this site signifies your acceptance of the <a href="https://www.w3.org/">Terms and Conditions</a></p>
+    <p class = "BottomText">
+         <a href="https://www.w3.org/">Privacy Policy |</a> 
+        <a href="https://www.w3.org/"> Contact Us |</a>
+        <a href="https://www.w3.org/"> About Us </a>
+
+    </div>
+
+</body>
+</html>
