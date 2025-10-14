0722fc9567d08e9c19742aeea7d95162be251ac7
ThePlayerRolo <thesolis78243@gmail.com>
2025-10-14 18:31:07 -0400

    Rendering :tada:

--- resources/views/assets/test.php
+++ resources/views/assets/test.php
@@ -1,9 +1,26 @@
 <?php
 
 include(base_path('ROCHAO-RCCServiceSoap\Assemblies\Roblox\Grid\RCC_Include.php'));
+use Illuminate\Support\Facades\Storage;
 
-$RCCServiceSoap = new Roblox\Grid\Rcc\RCCServiceSoap("127.0.0.1", 64989);
+use Roblox\Grid\Rcc as RCC;
 
-echo("HelloWorld(): ".($RCCServiceSoap->HelloWorld() ?? "Failed!")."\n");
+$RCCServiceSoap = new RCC\RCCServiceSoap("127.0.0.1", 64989);
+
+$job = new RCC\Job("StringTest4");
+$scriptData = file_get_contents(storage_path('RenderingScripts\HatThumbnail.lua'));
+$scriptData  = str_replace("%asset%", 1, $scriptData);
+echo $scriptData;
+$script = new RCC\ScriptExecution("StringTest4-Script", $scriptData);
+$text =  $RCCServiceSoap->OpenJob($job, $script);
+
+
+$RCCServiceSoap->CloseJob("StringTest4");
+if ($text != null) {
+    $image_encoded =  $text[0];
+    $image = base64_decode($image_encoded);
+
+    Storage::put('test.png', $image);
+}
 
 ?>
---
+++ storage/RenderingScripts/HatThumbnail.lua
@@ -0,0 +1,2 @@
+game:GetObjects("http://www.rochao.xyz/asset/?id=%asset%")[1].Parent = workspace
+return game:GetService("ThumbnailGenerator"):Click("PNG", 250, 250, true)
